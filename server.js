127bba126ab8236a24663a8c1a7c2ffead0c4d8f
Agata Wisniewska <ag.e.wisniewska@gmail.com>
2016-06-12 16:44:05 +0200

    heroku server fixed

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
-// server.js
+var express = require('express');
+var app = express();
 
-    // set up ========================
-    var express  = require('express');
-    var app      = express();                               // create our app w/ express
-     // log requests to the console (express4)
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
-    var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+app.set('port', (process.env.PORT || 5000));
 
-    // configuration =================
+app.use(express.static(__dirname + '/'));
 
-   // connect to mongoDB database on modulus.io
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-    app.use(express.static(__dirname + '/'));                 // set the static files location /public/img will be /img for users
-    // app.use(morgan('dev'));                                         // log every request to the console
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-    app.use(methodOverride());
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
 
-    // listen (start app with node server.js) ======================================
-    app.listen(8080);
-    console.log("App listening on port 8080");
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
