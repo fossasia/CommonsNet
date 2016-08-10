fde53c8485f8e2f8022a8c56bacfba036baca591
Agata Wiśniewska <ag.e.wisniewska@gmail.com>
2016-08-10 22:44:33 +0200

    modyfing fodt file first part

--- js/website.js
+++ js/website.js
@@ -217,93 +217,116 @@ angular.module('website', ['ngRoute', 'summernote']).
         
         // function save to replacing values in fodt file based on different wizard form fields. 
         vm.save = function() {
+          var table = [];
                $http({
             method: 'GET',
-            url: 'https://commonsnet.herokuapp.com/generatefile.fodt',
+            url: 'http://127.0.0.1:8083/generatefile.fodt',
            
         }).success(function(data){
+          console.log(vm.securitytypes)
 
-            // With the data succesfully returnd, call our callback
-          
 
-            var result = data.replace("INPUTS", vm.ssid);
-            var result  = result.replace("INPUTP", vm.password);
-            var result  = result.replace("INPUTA", vm.securitytypes);
-             var result  = result.replace("863", vm.capacity); 
-              var result  = result.replace("INPUTT", vm.wifistandards);
-
-              if (vm.paymentfieldyes ==='yes') {
-             var result  = result.replace("PAIDFIELD", "YES" );
+            var result = data.replace("INPUT_SSID", vm.ssid);
+              
+            if ((vm.password !== "") && (typeof vm.password !== "undefined")) {
+             result = result.replace("INPUT_PASSWORD", "The owner informs that password is" + " " + vm.password);
+           
               }
+             else {
+              result = result.replace("INPUT_PASSWORD", "The owner declars that there is no password")
+             }
+
+              if ((vm.securitytypes !== "") && (typeof vm.securitytypes !== "undefined") && (vm.securitytypes !== "OPEN")) {
+              result  = result.replace("SECURITY_TYPE", "The owner informs that network is secured under" + " " + vm.securitytypes);
+           
+              }
+              if((vm.securitytypes === "OPEN") && (vm.securitytypes !== "") && (typeof vm.securitytypes !== "undefined") )
+                result = result.replace("SECURITY_TYPE", "The owner declares that the network is" + " " + vm.securitytypes)
+             else {
+               result = result.replace("SECURITY_TYPE", "The owner declars that the network is unsecured")
+             }
+
+              if((vm.wifistandards !== "") && (typeof vm.wifistandards !== "undefined") )
+                result = result.replace("STANDARD_WIFI", "The owner declares that the network uses" + " " + vm.wifistandards)
+             else {
+               result = result.replace('<text:p text:style-name="P115">STANDARD_WIFI</text:p>', '')
+             }
+           
+           
+       
+             
+          if (vm.paymentfieldyes ==='yes') {
+              result  = result.replace("PAYMENT_FIELD", "The owner declares that Wifi network is paid");
+              } 
               else {
-           var result  = result.replace("PAIDFIELD", " - " );
+           result  = result.replace("PAYMENT_FIELD", "The owner declares that Wifi connection is completely free of any charge.");
 
               } 
               if (vm.paymentfieldyes ==='yes') {
-             var result  = result.replace("HOWFIELD", vm.paymentfield);
+             result  = result.replace("FEE_FIELD", "The fee is" + " " + vm.paymentfield);
               }
               else {
-              var result  = result.replace("HOWFIELD", " - ");
+             result  = result.replace('<text:p text:style-name="P116">FEE_FIELD</text:p>' , '');
 
               }
             if (vm.timelimityes ==='yes') {
-             var result  = result.replace("800", "YES");
+             result  = result.replace("LIMIT_FIELD", "The owner informs that access to the network is limited");
               }
               else {
-            var result  = result.replace("800", " - ");
+            result  = result.replace("LIMIT_FIELD", "The owner provides unlimited access to the network");
 
               }
  
             if (vm.timelimityes ==='yes') {
-             var result  = result.replace("HOWTIME", vm.timelimitfield);
+             result  = result.replace("TIME_LIMIT", "The owner declares that you are able to use the network" + " " + vm.timelimitfield);
               }
               else {
-            var result  = result.replace("HOWTIME", " - ");
+            result  = result.replace('<text:h text:style-name="P140" text:outline-level="3"><text:span text:style-name="T108">TIME_LIMIT</text:span></text:h>', ' ');
 
               }
 
-            if (vm.specialdevices ==='yes') {
-                var result  = result.replace("452", 'Special devices');
-                  }
-            else {
-               var result  = result.replace("452", " ");
+         //    if (vm.specialdevices ==='yes') {
+         //        var result  = result.replace("452", 'Special devices');
+         //          }
+         //    else {
+         //       var result  = result.replace("452", " ");
 
-                  } 
+         //          } 
            
-            if (vm.specialsettings ==='yes') {
-                var result  = result.replace("163", 'Special settings');
-                  }
-            else {
-               var result  = result.replace("163", " ");
+         //    if (vm.specialsettings ==='yes') {
+         //        var result  = result.replace("163", 'Special settings');
+         //          }
+         //    else {
+         //       var result  = result.replace("163", " ");
 
-                  } 
+         //          } 
             
-            if (vm.acceptterms ==='yes') {
-                var result  = result.replace("365", 'Accepting terms of use');
-                  }
-            else {
-               var result  = result.replace("365", " ");
+         //    if (vm.acceptterms ==='yes') {
+         //        var result  = result.replace("365", 'Accepting terms of use');
+         //          }
+         //    else {
+         //       var result  = result.replace("365", " ");
 
-                  } 
-            if (vm.socialprofile ==='yes') {
-                var result  = result.replace("186", 'Liking social profile');
-                  }
-            else {
-               var result  = result.replace("186", " ");
+         //          } 
+         //    if (vm.socialprofile ==='yes') {
+         //        var result  = result.replace("186", 'Liking social profile');
+         //          }
+         //    else {
+         //       var result  = result.replace("186", " ");
 
 
-                  } 
-                  if (vm.downloading ==='yes') {
-                var result  = result.replace("236", 'Downloading pdf file');
-                  }
-            else {
-               var result  = result.replace("236", " ");
+         //          } 
+         //          if (vm.downloading ==='yes') {
+         //        var result  = result.replace("236", 'Downloading pdf file');
+         //          }
+         //    else {
+         //       var result  = result.replace("236", " ");
 
-                  } 
+         //          } 
          
-          var result = result.replace("888", vm.countires);
-          var result = result.replace("456", vm.legalrestrictions);
-         console.log(result)
+         //  var result = result.replace("888", vm.countires);
+         //  var result = result.replace("456", vm.legalrestrictions);
+         // console.log(result)
 
 
 
@@ -314,11 +337,13 @@ angular.module('website', ['ngRoute', 'summernote']).
         }).error(function(){
             alert("error");
         });
-   }
+   
 
 
+      
  
-    })
+    }
+  })
 
 
      
