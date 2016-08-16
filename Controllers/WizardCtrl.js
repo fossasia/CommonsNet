f325a8a716195d58db78d9752b9aba63d02c3069
Agata Wiśniewska <ag.e.wisniewska@gmail.com>
2016-08-16 18:50:59 +0200

    generating a file feature and .fodt file changes

--- Controllers/WizardCtrl.js
+++ Controllers/WizardCtrl.js
@@ -109,7 +109,7 @@
           var table = [];
                $http({
             method: 'GET',
-            url: 'http://commonsnet.herokuapp.com/generatefile.fodt',
+            url: 'http://127.0.0.1:8083/generatefile.fodt',
            
         }).success(function(data){
           console.log(vm.securitytypes);
@@ -117,7 +117,7 @@
 
 
             var result = data.replace("INPUT_SSID", vm.ssid);
-            result = result.replace("NETWORK_NAME", "The owner provides" + " "  + vm.ssid + "network connection")
+            result = result.replace("NETWORK_NAME", "The owner provides" + " "  + vm.ssid +  " " + "network connection")
               
             if ((vm.password !== "") && (typeof vm.password !== "undefined")) {
              result = result.replace("INPUT_PASSWORD", "The owner informs that password is" + " " + vm.password);
@@ -140,14 +140,14 @@
               if((vm.wifistandards !== "") && (typeof vm.wifistandards !== "undefined") )
                 result = result.replace("STANDARD_WIFI", "The owner declares that the network uses" + " " + vm.wifistandards + "" + "standard")
              else {
-               result = result.replace('<text:p text:style-name="P115">STANDARD_WIFI</text:p>', '')
+               result = result.replace('<text:p text:style-name="P149">STANDARD_WIFI</text:p>', '')
              }
               if ((vm.dualbandyes !== "") && (typeof vm.dualbandyes !== "undefined")) {
              result = result.replace("BANDWIDTH_CONTROL", "The Owner declares to provide a bandwidth control");
            
               }
               else {
-                result.result.replace('<text:p text:style-name="P115">BANDWIDTH_CONTROL</text:p>', '')
+                result= result.replace('<text:p text:style-name="P149">BANDWIDTH_CONTROL</text:p>', '')
               }
            
            
@@ -164,7 +164,7 @@
              result  = result.replace("FEE_FIELD", "The fee is" + " " + vm.paymentfield);
               }
               else {
-             result  = result.replace('<text:p text:style-name="P116">FEE_FIELD</text:p>' , 'The owner declares that he does not require any fee');
+             result  = result.replace(' <text:p text:style-name="P150">FEE_FIELD</text:p>' , 'The owner declares that he does not require any fee');
 
               }
             if (vm.timelimityes ==='yes') {
@@ -179,7 +179,7 @@
 
               }
               else {
-                result = result.replace('<text:h text:style-name="P140" text:outline-level="3"><text:span text:style-name="T108">TIME_LIMIT</text:span></text:h>', " ")
+                result = result.replace(' <text:h text:style-name="P167" text:outline-level="3">TIME_LIMIT</text:h>', " ")
               }
  
             
@@ -193,58 +193,120 @@
 
               }
 
-            if (vm.specialdevices === "yes" || vm.specialsettings === "yes" || vm.socialprofile === "yes" || vm.acceptterms === "yes" || vm.socialprofile === "yes" || vm.downloading === "yes") {
-              result = result.replace("CONDITIONS", "The owner declares that there are some requirments to use Wifi")
+            if (vm.specialdevices === "yes" || vm.specialsettings === "yes" || vm.socialprofile === "yes" || vm.acceptterms === "yes" || vm.socialprofile === "yes" || vm.downloading === "yes" || vm.register === "yes" || vm.newsletter === "yes" || vm.mobilenumber === "yes" || vm.emailaddress === "yes" || vm.personaldetails === "yes" || vm.autoupdatedisabled === "yes" || vm.referencenumber === "yes" ) {
+              result = result.replace("CONDITIONS", "The Owner declares that there are some requirments to use Wifi")
             }
             else {
-              result = result. replace("CONDITIONS", "The owner declares that there is no requirments to use Wifi")
-              result = result.replace(' <text:h text:style-name="P135" text:outline-level="3">To use Wifi you are required to SPECIAL_DEVICES SPECIAL_SETTINGS ACCEPT_TERMS LIKE DOWNLOAD</text:h>', ' ' )
+              result = result. replace("CONDITIONS", "The Owner declares that there are not any requirmenets to use Wifi")
+              result = result.replace('<text:h text:style-name="P164" text:outline-level="3">The Owner informs that to use Wifi the User is required to:</text:h>', ' ')
+              result = result.replace('<text:list xml:id="list533319923915382650" text:style-name="L2"><text:list-item><text:p text:style-name="P155">SPECIAL_DEVICES</text:p></text:list-item><text:list-item><text:p text:style-name="P155">SPECIAL SETTINGS</text:p></text:list-item><text:list-item><text:p text:style-name="P155">ACCEPT_TERMS</text:p></text:list-item><text:list-item><text:p text:style-name="P155">LIKE</text:p></text:list-item><text:list-item><text:p text:style-name="P155">DOWNLOAD</text:p></text:list-item> <text:list-item><text:p text:style-name="P155">REGISTER</text:p></text:list-item><text:list-item><text:p text:style-name="P155">NEWSLETTER</text:p></text:list-item><text:list-item><text:p text:style-name="P155">MOBILE_NUMER</text:p></text:list-item> <text:list-item><text:p text:style-name="P155">EMAIL_ADDRESS</text:p></text:list-item><text:list-item><text:p text:style-name="P155">PERSONAL_DETAILS</text:p></text:list-item><text:list-item><text:p text:style-name="P155">DISABLE_FUNCTION</text:p></text:list-item><text:list-item><text:p text:style-name="P155">REFERENCE_NUMBER</text:p> </text:list-item></text:list>', '')
               }
             
             if (vm.specialdevices ==='yes') {
-                var result  = result.replace("SPECIAL_DEVICES", 'use special devices like' + ' ' + vm.specialdevicesfield+",");
+                var result  = result.replace("SPECIAL_DEVICES", 'use special devices like' + ' ' + vm.specialdevicesfield );
                   }
             else {
-               var result  = result.replace("SPECIAL_DEVICES", " ");
+               var result  = result.replace('<text:p text:style-name="P155">SPECIAL_DEVICES</text:p>', ' ');
 
                   } 
 
              if (vm.specialsettings ==='yes') {
-                var result  = result.replace("SPECIAL_SETTINGS", 'run special settings like' + ' ' + vm.specialsettingsfield+ ",");
+                var result  = result.replace("SPECIAL_SETTINGS", 'run special settings like' + ' ' + vm.specialsettingsfield);
                   }
             else {
-               var result  = result.replace("SPECIAL_SETTINGS", " ");
+               var result  = result.replace('<text:p text:style-name="P155">SPECIAL SETTINGS</text:p>', " ");
 
                   } 
 
              if (vm.acceptterms ==='yes') {
-                var result  = result.replace("ACCEPT_TERMS", 'accept terms of use' + ",");
+                var result  = result.replace("ACCEPT_TERMS", 'accept terms of use');
                   }
             else {
-               var result  = result.replace("ACCEPT_TERMS", " ");
+               var result  = result.replace('<text:p text:style-name="P155">ACCEPT_TERMS</text:p>', " ");
 
                   } 
-            
 
+      
             if (vm.socialprofile ==='yes') {
-                var result  = result.replace("LIKE", 'like social profile' + ",");
+                var result  = result.replace("LIKE", 'like social profile');
                   }
             else {
-               var result  = result.replace("LIKE", " ");
+               var result  = result.replace('<text:p text:style-name="P155">LIKE</text:p>', " ");
 
 
                   } 
             if (vm.downloading ==='yes') {
-                var result  = result.replace("DOWNLOAD", ', downloading pdf file'+ ".");
+                var result  = result.replace("DOWNLOAD", 'download terms of use pdf file');
                   }
             else {
-               var result  = result.replace("DOWNLOAD", " ");
+               var result  = result.replace('<text:p text:style-name="P155">DOWNLOAD</text:p>', " ");
 
                   } 
-         
+            if (vm.register === 'yes') {
+              var result = result.replace("REGISTER", 'register to website to use Wifi')
+            }
+            else {
+              var result = result.replace('<text:p text:style-name="P155">REGISTER</text:p>', ' ')
+            }
+            if (vm.newsletter === 'yes') {
+              var result = result.replace("NEWSLETTER", 'sign up to a newsletter')
+            }
+            else {
+              result = result.replace('<text:p text:style-name="P155">NEWSLETTER</text:p>', '')
+            }
+            if (vm.mobilenumber === 'yes') {
+              var result = result.replace("MOBILE_NUMER", 'give a mobile number')
+            }
+            else  {
+              result = result.replace('<text:p text:style-name="P155">MOBILE_NUMER</text:p>', '')
+            }
+
+            if (vm.emailaddress === 'yes') {
+              result = result.replace("EMAIL_ADDRESS", 'give an email address')
+            }
+            else { 
+              result = result.replace('<text:p text:style-name="P155">EMAIL_ADDRESS</text:p>', '') 
+            }
+             if (vm.personaldetails === 'yes') {
+              result = result.replace("PERSONAL_DETAILS", 'provide personal details like age, gender etc.')
+            }
+            else { 
+              result = result.replace('<text:p text:style-name="P155">PERSONAL_DETAILS</text:p>', '') 
+            }
+            if (vm.autoupdatedisabled === 'yes') {
+              result = result.replace("DISABLE_FUNCTION", 'disable auto-update function')
+            }
+            else {
+              result = result.replace('<text:p text:style-name="P155">DISABLE_FUNCTION</text:p>', ' ')
+            }
+            if (vm.referencenumber === 'yes') {
+                result = result.replace("REFERENCE_NUMBER", 'have a reference number')
+            }
+            else {
+              result = result.replace('<text:p text:style-name="P155">REFERENCE_NUMBER</text:p>', ' ')
+            }
+
+           if (vm.ownrestrictionyes === "yes") {
+            result = result.replace("OWN_RESTRICTIONS", "The Owner informs that the Users are forbidden to" + vm.ownrestrictionsfield);
+        
+            }
+            else {
+              result = result.replace('<text:p text:style-name="P150">OWN_RESTRICTIONS</text:p>', ' ')
+            
+          }
+          if (vm.limitdevicesyes === "yes") {
+            result = result.replace("NUMBER_OF_DEVICES", "The Owner informs that the Users are forbidden to use more than" + ' ' + vm.limitdevicesfield + ' ' + 'devices');
+        
+            }
+            else {
+              result = result.replace('<text:p text:style-name="P161">NUMBER_OF_DEVICES</text:p>', ' ')
+            
+          }
+          
           if (vm.country !== "yes") {
-            result = result.replace("LEGAL_RESTRICTIONS", "The owner informs that there are not known to him any legal restriction to use Wifi connection, using Internet resources or taking actions in the network in"+ " " + vm.countries);
-            result = result.replace('<text:p text:style-name="P118">FIELD_RESTRICTIONS</text:p>', ' ')
+            result = result.replace("LEGAL_RESTRICTIONS", "The owner informs that there are not known to him any legal restriction to use Wifi connection, using Internet resources or taking actions in the network.");
+            result = result.replace('<text:p text:style-name="P151">LEGAL_RESTRICTIONS</text:p>', ' ')
+            result = result.replace('<text:p text:style-name="P152">FIELD_RESTRICTIONS</text:p>', ' ')
           }
           else {
             result = result.replace("LEGAL_RESTRICTIONS", "The owner declares that the law of" + " " + vm.countries + " " + "prohibits:")
